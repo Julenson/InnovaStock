3cdeab7ee04165c74fcb5b3e9fd6d9c17cac83a3
Julen Haro <julenson50@gmail.com>
2025-07-22 10:07:09 +0000

    comas

--- db/managedb.ts
+++ db/managedb.ts
@@ -28,11 +28,13 @@ export async function initDatabase() {
       email VARCHAR(255) NOT NULL,
       password VARCHAR(255) NOT NULL,
       category VARCHAR(255),
-      INSERT INTO users (mail, password)
-      VALUES ('demo@innovasport.com', 'demopassword')
-      ON CONFLICT (mail) DO NOTHING;
     );
   `;
+
+  await sql`
+  INSERT INTO users (email,password)
+  VALUES (demo@innovasport.com,demopassword)
+  ON CONFLICT (email) DO NOTHING`
 
   console.log('Database initialized.');
 }
